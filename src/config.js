4568b30ba520167b0f4142d636955a21f84b0e5d
Joshua Earnest <joshuaearnest@JPs-MacBook-Pro.fios-router.home>
2020-05-01 15:52:58 -0400

    adding basic routing

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const region = "us-east-1";
+
+export default {
+  s3: {
+    REGION: region,
+    BUCKET: "jp-east-1-serverless-tutorial"
+  },
+  apiGateway: {
+    REGION: region,
+    URL: "https://ve6yk4j6x0.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: region,
+    USER_POOL_ID: "us-east-1_TSBlBspbj",
+    APP_CLIENT_ID: "1chai72anfr4aj2etadmh97jjm",
+    IDENTITY_POOL_ID: "us-east-1:53a1ea41-7a48-4a76-9103-d353b21ace47"
+  }
+};
